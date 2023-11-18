ca291f556cd4a2916ec843309958f10619ed523c
Gabriel Peter <gpeterferreira@gmail.com>
2023-11-18 13:23:52 -0800

    testando

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,32 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    public String cidade;
+    private String uf;
+    private String cep;
+    private String numero;
+    private String complemento;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+        this.cidade = dados.cidade();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
+
+}
