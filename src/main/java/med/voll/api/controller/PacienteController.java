1050eb652b595e6acaffe791ff7568fb45bf735d
Gabriel Peter <gpeterferreira@gmail.com>
2023-11-22 22:13:29 -0800

    adicionando buscar por id

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,14 @@
 package med.voll.api.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,10 +37,24 @@ public class PacienteController {
 
     @GetMapping()
     public Page<Object> Listar(@PageableDefault(size = 2, sort = { "cpf" }) Pageable paginacao) {
-       
+
         return repository.findAll(paginacao).map(DadosListagemPaciente::new);
-        
+
     }
+    
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void Deletar(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+    
+    @GetMapping("/buscarid/{id}")
+    public Optional<Paciente> Listar(@PathVariable Long id) {
+
+        return repository.findById(id);
+
+    }
+    
 }
 
 
